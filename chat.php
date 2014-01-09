d40e83e400103c733f1b9ed20ba1922502b4fdc1
likebin <speedneeder@163.com>
2014-01-10 03:42:21 +0800

    0.12 增加了message窗口提示用户进入和离开

--- chat.php
+++ chat.php
@@ -8,8 +8,8 @@ $mysql = new SaeMysql();
 $uid = $_COOKIE['uid'];
 if (empty($uid))
 {
-	$uid = mt_rand();
-	setcookie('uid', $uid, time()+3000, '/', 'chatbox.sinaapp.com', FALSE, TRUE);
+    $uid = mt_rand();
+    setcookie('uid', $uid, time()+3000, '/', 'chatbox.sinaapp.com', FALSE, TRUE);
 }
 
 //用户列表保存在kvdb之中
@@ -30,12 +30,12 @@ if($act == 'create_channel')
     $username = $_REQUEST["username"];
     $user_list[$uid] = $username;    
     
-   	// channel 名称
+    // channel 名称
     $channel_name = 'chatbox.'. $uid;
-	// 过期时间，默认为3600秒
+    // 过期时间，默认为3600秒
     $duration = 10;
-	//创建一个channel，返回一个地址
-	$channel_url = $channel->createChannel($channel_name, $duration);
+    //创建一个channel，返回一个地址
+    $channel_url = $channel->createChannel($channel_name, $duration);
 
     $kv->set('ChatingUserList', $user_list);
     
@@ -49,34 +49,34 @@ else if($act == 'sendmsg')
     $sql = "INSERT  INTO `msg_list` (msg, uname, action, send_time) VALUES ('$msg', '$username', 'msg', NOW());";
     $mysql->runSql( $sql );
     
-	$msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: blue">' . $username . '</span> 说: ' . $msg;
-	
+    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #5bc0de">' . $username . '</span> 说: ' . $msg;
     
-   	// 往名为$channel_name的channel里发送一条消息
-    foreach ($user_list as $uid => $uname) {
-		$channel_name = 'chatbox.'.$uid;
-		$send = $channel->sendMessage($channel_name, $msg);
-        if ($send !== TRUE){
-            unset($user_list[$uid]);
-        }		
-	}
-    
-    //向小风聊天机器人请求消息
-    $url= 'http://xiaofengrobot.sinaapp.com/api.php?text=' . $_REQUEST["message"];
-   	$answer= file_get_contents($url);
-    $sql = "INSERT  INTO `msg_list` (msg, uname, action, send_time) VALUES ('$answer', '小风', 'msg', NOW());";
-    $mysql->runSql( $sql );
-    
-    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: blue">小风</span> 说: ' . $answer;
     
     // 往名为$channel_name的channel里发送一条消息
     foreach ($user_list as $uid => $uname) {
-		$channel_name = 'chatbox.'.$uid;
-		$send = $channel->sendMessage($channel_name, $msg);
+        $channel_name = 'chatbox.'.$uid;
+        $send = $channel->sendMessage($channel_name, $msg);
         if ($send !== TRUE){
             unset($user_list[$uid]);
-        }		
-	}
+        }       
+    }
+    
+    //向小风聊天机器人请求消息
+    $url= 'http://xiaofengrobot.sinaapp.com/api.php?text=' . $_REQUEST["message"];
+    $answer= file_get_contents($url);
+    $sql = "INSERT  INTO `msg_list` (msg, uname, action, send_time) VALUES ('$answer', '小风', 'msg', NOW());";
+    $mysql->runSql( $sql );
+    
+    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #5bc0de">小风</span> 说: ' . $answer;
+    
+    // 往名为$channel_name的channel里发送一条消息
+    foreach ($user_list as $uid => $uname) {
+        $channel_name = 'chatbox.'.$uid;
+        $send = $channel->sendMessage($channel_name, $msg);
+        if ($send !== TRUE){
+            unset($user_list[$uid]);
+        }       
+    }
     
     $mysql->closeDb();
 }
@@ -84,27 +84,27 @@ else if($act == 'sendmsg')
 else if($act == 'sendto') {
     $msg = $_REQUEST["message"];
     $username = $_REQUEST["username"]; 
-    $sendto = $_REQUEST["to"];    	
+    $sendto = $_REQUEST["to"];      
     
-   	//私聊则只发给指定用户
+    //私聊则只发给指定用户
     foreach ($user_list as $uid => $uname) {
         if($uname == $sendto) {
-            $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: purple"> ' . $username . ' 悄悄对你说</span>: ' . $_REQUEST["message"];
-        	$channel_name = 'chatbox.'.$uid;
-			$send = $channel->sendMessage($channel_name, $msg);
-	        if ($send !== TRUE){
-            	unset($user_list[$uid]);
-        	}            
+            $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #563d7c"> ' . $username . ' 悄悄对你说</span>: ' . $_REQUEST["message"];
+            $channel_name = 'chatbox.'.$uid;
+            $send = $channel->sendMessage($channel_name, $msg);
+            if ($send !== TRUE){
+                unset($user_list[$uid]);
+            }            
         }
         else if($uname == $username) {
-            $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: purple">你悄悄对 ' . $sendto . ' 说</span>: ' . $_REQUEST["message"];
-        	$channel_name = 'chatbox.'.$uid;
-			$send = $channel->sendMessage($channel_name, $msg);
-	        if ($send !== TRUE){
-            	unset($user_list[$uid]);
-        	}            
+            $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #563d7c">你悄悄对 ' . $sendto . ' 说</span>: ' . $_REQUEST["message"];
+            $channel_name = 'chatbox.'.$uid;
+            $send = $channel->sendMessage($channel_name, $msg);
+            if ($send !== TRUE){
+                unset($user_list[$uid]);
+            }            
         }
-	} 
+    } 
     
 }
 //获取当前用户列表
@@ -147,18 +147,18 @@ else if($act == 'getmsglist')
 else if($act == 'connected')
 {
     $username = $_REQUEST["username"];
-    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: green">' . $username . ' 进入聊天室</span>';
+    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #5cb85c">' . $username . ' 进入聊天室</span>';
 
     $sql = "INSERT  INTO `msg_list` (msg, uname, action, send_time) VALUES ('', '$username', 'open', NOW());";
     $mysql->runSql( $sql );
     $mysql->closeDb();
-	//向列表之中的所有用户发送消息
-   	foreach ($user_list as $uid => $uname) {
-		$channel_name = 'chatbox.'.$uid;
-		$send = $channel->sendMessage($channel_name, $msg);
+    //向列表之中的所有用户发送消息
+    foreach ($user_list as $uid => $uname) {
+        $channel_name = 'chatbox.'.$uid;
+        $send = $channel->sendMessage($channel_name, $msg);
         if ($send !== TRUE){
             unset($user_list[$uid]);
-        }		
+        }       
     }  
     $kv->set('ChatingUserList', $user_list);    
 }
@@ -166,17 +166,17 @@ else if($act == 'disconnected')
 {    
     unset($user_list[$uid]);
     $username = $_REQUEST["username"];
-    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: red">' . $username . ' 退出聊天室</span>';
+    $msg = date('Y-m-d H:m:s') . ' <span style="color: white; background-color: #d9534f">' . $username . ' 退出聊天室</span>';
     $sql = "INSERT  INTO `msg_list` (msg, uname, action, send_time) VALUES ('', '$username', 'close', NOW());";
     $mysql->runSql( $sql );
     $mysql->closeDb();
     //向列表之中的所有用户发送消息
     foreach ($user_list as $uid => $uname) {
-		$channel_name = 'chatbox.'.$uid;
-		$send = $channel->sendMessage($channel_name, $msg);
+        $channel_name = 'chatbox.'.$uid;
+        $send = $channel->sendMessage($channel_name, $msg);
         if ($send !== TRUE){
             unset($user_list[$uid]);
-        }		
+        }       
     } 
     $kv->set('ChatingUserList', $user_list);
 }
